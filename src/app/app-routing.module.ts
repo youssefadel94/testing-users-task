14fb23e71d2a594ab32db25ecf9ac4c9dfb52ad2
Youssef adel <darkmetalxdragon@hotmail.com>
2021-10-05 18:19:23 +0200

    add route and finalize service

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { QueryComponent } from './components/query/query.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '*', redirectTo: '/query', pathMatch: 'full' },
+  { path: 'query', component: QueryComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/services/test-users-and-questions.service.ts
+++ src/app/services/test-users-and-questions.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { IQuestion, IResponse, IUser } from '../interfaces/users-and-questions';
 
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json',
+  }),
+};
 @Injectable({
   providedIn: 'root'
 })
 export class TestUsersAndQuestionsService {
+  private apiUrl = 'http://localhost:5000';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getQuestions(): Observable<IQuestion[]> {
+    return this.http.get<IQuestion[]>(`${this.apiUrl}/IQuestion`);
+  }
+  getResponses(QId: string, RId: number): Observable<IResponse[]> {
+    return this.http.get<IResponse[]>(`${this.apiUrl}/IResponse?questionId=${QId}&answeredIndex=${RId}`);
+  }
+  getUser(id:string): Observable<IUser> {
+    return this.http.get<IUser>(`${this.apiUrl}/IUser?id=${id}`);
+  }
+  getUsers(): Observable<Task[]> {
+    return this.http.get<Task[]>(`${this.apiUrl}/IUser`);
+  }
 }
