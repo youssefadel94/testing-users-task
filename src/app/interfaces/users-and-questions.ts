bdce7c38494f3b8dc9e6adf9967414880bd258b4
Youssef adel <darkmetalxdragon@hotmail.com>
2021-10-05 18:03:28 +0200

    create mock data and interface

--- src/app/interfaces/users-and-questions.ts
+++ src/app/interfaces/users-and-questions.ts
@@ -1,2 +1,18 @@
 export interface UsersAndQuestions {
+    
+}
+
+export interface IUser {
+    id:string,
+    email:string
+}
+export interface IQuestion {
+    id:string,
+    question:string,
+    answers:string[],
+}
+export interface IResponse {
+    userId:string,
+    questionId:string,
+    answeredIndex:number
 }
